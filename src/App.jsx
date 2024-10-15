ee8a200341865aa1f733a998b28c1366f6b7df0a
Lee Grobler <hello@lee-grobler.com>
2024-10-15 15:51:58 +0200

    added environment variables.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Header from "./components/Header";
 
 export default function App() {
+  const apiKey = import.meta.env.VITE_API_KEY;
+
   return (
     <>
       <Header title="Header"></Header>
 
       <main className="p-4">
         <p className="text-3xl font-bold underline uppercase">hello world</p>
+        <p>api key: {apiKey}</p>
       </main>
     </>
   );
